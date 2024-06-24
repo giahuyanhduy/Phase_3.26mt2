15176dd7890e3df0f25513f36519e438db380af0
giahuyanhduy <giahuyanhduy@gmail.com>
2024-06-24 09:22:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ curl -L -o /tmp/Phase_3.zip https://github.com/giahuyanhduy/phase/blob/main/Phas
 # Kiểm tra xem thư mục /home/giang có tồn tại hay không
 if [ -d "/home/giang" ]; then
     # Nếu tồn tại, giải nén vào /home/giang
-    unzip -o /tmp/Phase_3.zip -d /home/giang/
+    unzip -o /tmp/Phase_3.zip -d /home/giang/Phase_3
     # Truy cập vào thư mục Phase_3
     cd /home/giang/Phase_3 || exit
 else
     # Nếu không tồn tại, giải nén ra /home
-    unzip -o /tmp/Phase_3.zip -d /home/
+    unzip -o /tmp/Phase_3.zip -d /home/Phase_3
     # Truy cập vào thư mục Phase_3
     cd /home/Phase_3 || exit
 fi
